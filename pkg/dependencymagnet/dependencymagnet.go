a1f2929aa1d54b901a6816b2397f0901b4954b4e
Christian Huffman <chuffman@redhat.com>
2020-07-16 16:48:01 -0400

    Updated snapshot-controller version and dependency versions

--- pkg/dependencymagnet/dependencymagnet.go
+++ pkg/dependencymagnet/dependencymagnet.go
@@ -5,6 +5,7 @@
 package dependencymagnet
 
 import (
-	_ "github.com/jteeuwen/go-bindata/go-bindata"
-	_ "github.com/openshift/library-go/alpha-build-machinery"
+	_ "github.com/go-bindata/go-bindata"
+	_ "github.com/go-bindata/go-bindata/go-bindata"
+	_ "github.com/openshift/build-machinery-go"
 )
