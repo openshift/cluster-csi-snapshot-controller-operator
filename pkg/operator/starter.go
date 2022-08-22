e2c455cdb1a43d319ba5f3a880c59040036cca81
Jan Safranek <jsafrane@redhat.com>
2022-08-22 17:08:54 +0200

    Add changes for HyperShift
    Use guest cluster nodes to calculate replica count
    
    To prevent listing + watching nodes in the management cluster.

--- pkg/operator/starter.go
+++ pkg/operator/starter.go
@@ -1,6 +1,7 @@
 package operator
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"os"
@@ -12,6 +13,7 @@ import (
 	configclient "github.com/openshift/client-go/config/clientset/versioned"
 	configinformer "github.com/openshift/client-go/config/informers/externalversions"
 	"github.com/openshift/cluster-csi-snapshot-controller-operator/assets"
+	"github.com/openshift/library-go/pkg/config/client"
 	"github.com/openshift/library-go/pkg/controller/controllercmd"
 	"github.com/openshift/library-go/pkg/controller/factory"
 	"github.com/openshift/library-go/pkg/operator/csi/csidrivercontrollerservicecontroller"
@@ -25,6 +27,9 @@ import (
 	"github.com/openshift/library-go/pkg/operator/staticresourcecontroller"
 	"github.com/openshift/library-go/pkg/operator/status"
 	"github.com/openshift/library-go/pkg/operator/v1helpers"
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
 	apiextclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	kubeclient "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -33,9 +38,8 @@ import (
 )
 
 const (
-	targetName        = "csi-snapshot-controller"
-	targetNamespace   = "openshift-cluster-storage-operator"
-	operatorNamespace = "openshift-cluster-storage-operator"
+	targetName              = "csi-snapshot-controller"
+	defaultOperandNamespace = "openshift-cluster-storage-operator"
 
 	operatorVersionEnvName = "OPERATOR_IMAGE_VERSION"
 	operandVersionEnvName  = "OPERAND_IMAGE_VERSION"
@@ -45,55 +49,90 @@ const (
 	resync = 20 * time.Minute
 )
 
-func RunOperator(ctx context.Context, controllerConfig *controllercmd.ControllerContext) error {
-	kubeClient, err := kubeclient.NewForConfig(rest.AddUserAgent(controllerConfig.KubeConfig, targetName))
+func RunOperator(ctx context.Context, controllerConfig *controllercmd.ControllerContext, guestKubeConfigFile string) error {
+	isHyperShift := guestKubeConfigFile != ""
+	// Kubeconfig received through service account or --kubeconfig arg
+	// is for the management cluster.
+	managementKubeConfig := controllerConfig.KubeConfig
+	managementKubeClient, err := kubeclient.NewForConfig(rest.AddUserAgent(managementKubeConfig, targetName))
 	if err != nil {
 		return err
 	}
-	kubeInformersForNamespaces := v1helpers.NewKubeInformersForNamespaces(kubeClient, "", operatorNamespace, targetNamespace)
+	operandNamespace := defaultOperandNamespace
+	// Guest kubeconfig is the same as the management cluster one unless guestKubeConfigFile is provided
+	guestKubeClient := managementKubeClient
+	guestKubeConfig := managementKubeConfig
+	if isHyperShift {
+		guestKubeConfig, err = client.GetKubeConfigOrInClusterConfig(guestKubeConfigFile, nil)
+		if err != nil {
+			return fmt.Errorf("failed to use guest kubeconfig %s: %s", guestKubeConfigFile, err)
+		}
+		operandNamespace = controllerConfig.OperatorNamespace
+		guestKubeClient = kubeclient.NewForConfigOrDie(rest.AddUserAgent(guestKubeConfig, targetName))
+	}
 
-	configClient, err := configclient.NewForConfig(rest.AddUserAgent(controllerConfig.KubeConfig, targetName))
+	managementKubeInformersForNamespaces := v1helpers.NewKubeInformersForNamespaces(managementKubeClient, "", operandNamespace)
+	guestKubeInformersForNamespaces := v1helpers.NewKubeInformersForNamespaces(guestKubeClient, "", defaultOperandNamespace)
+
+	// config.openshift.io client - use the guest cluster (Infrastructure)
+	guestConfigClient, err := configclient.NewForConfig(rest.AddUserAgent(guestKubeConfig, targetName))
 	if err != nil {
 		return err
 	}
-	configInformers := configinformer.NewSharedInformerFactoryWithOptions(configClient, resync)
+	guestConfigInformers := configinformer.NewSharedInformerFactoryWithOptions(guestConfigClient, resync)
 
-	apiExtClient, err := apiextclient.NewForConfig(rest.AddUserAgent(controllerConfig.KubeConfig, targetName))
+	// CRD client - use the guest cluster
+	guestAPIExtClient, err := apiextclient.NewForConfig(rest.AddUserAgent(guestKubeConfig, targetName))
 	if err != nil {
 		return err
 	}
 
-	// Create GenericOperatorclient. This is used by the library-go controllers created down below
+	// Create GenericOperatorClient. This is used by the library-go controllers created down below
+	// Operator CR is in the guest cluster.
 	gvr := operatorv1.SchemeGroupVersion.WithResource("csisnapshotcontrollers")
-	operatorClient, dynamicInformers, err := goc.NewClusterScopedOperatorClientWithConfigName(controllerConfig.KubeConfig, gvr, "cluster")
+	guestOperatorClient, dynamicInformers, err := goc.NewClusterScopedOperatorClientWithConfigName(guestKubeConfig, gvr, "cluster")
 	if err != nil {
 		return err
 	}
 
 	versionGetter := status.NewVersionGetter()
 
-	staticResourcesController := staticresourcecontroller.NewStaticResourceController(
-		"CSISnapshotStaticResourceController",
-		assets.ReadFile,
+	namespacedAssetFunc := namespaceReplacer(assets.ReadFile, operandNamespace)
+	guestStaticResourceController := staticresourcecontroller.NewStaticResourceController(
+		"CSISnapshotGuestStaticResourceController",
+		namespacedAssetFunc,
 		[]string{
 			"volumesnapshots.yaml",
 			"volumesnapshotcontents.yaml",
 			"volumesnapshotclasses.yaml",
+		},
+		resourceapply.NewKubeClientHolder(guestKubeClient).WithAPIExtensionsClient(guestAPIExtClient),
+		guestOperatorClient,
+		controllerConfig.EventRecorder,
+	)
+
+	mgmtStaticResourcesController := staticresourcecontroller.NewStaticResourceController(
+		"CSISnapshotPDBStaticResourceController",
+		namespacedAssetFunc,
+		[]string{
 			"serviceaccount.yaml",
-			"webhook_config.yaml",
 			"webhook_service.yaml",
 		},
-		resourceapply.NewKubeClientHolder(kubeClient).WithAPIExtensionsClient(apiExtClient),
-		operatorClient,
+		resourceapply.NewKubeClientHolder(managementKubeClient),
+		guestOperatorClient,
 		controllerConfig.EventRecorder,
 	).WithConditionalResources(
-		assets.ReadFile,
+		// Deploy PDBs everywhere except SNO
+		namespacedAssetFunc,
 		[]string{
 			"csi_controller_deployment_pdb.yaml",
 			"webhook_deployment_pdb.yaml",
 		},
 		func() bool {
-			isSNO, precheckSucceeded, err := staticcontrollercommon.NewIsSingleNodePlatformFn(configInformers.Config().V1().Infrastructures())()
+			if isHyperShift {
+				return true
+			}
+			isSNO, precheckSucceeded, err := staticcontrollercommon.NewIsSingleNodePlatformFn(guestConfigInformers.Config().V1().Infrastructures())()
 			if err != nil {
 				klog.Errorf("NewIsSingleNodePlatformFn failed: %v", err)
 				return false
@@ -105,7 +144,10 @@ func RunOperator(ctx context.Context, controllerConfig *controllercmd.Controller
 			return !isSNO
 		},
 		func() bool {
-			isSNO, precheckSucceeded, err := staticcontrollercommon.NewIsSingleNodePlatformFn(configInformers.Config().V1().Infrastructures())()
+			if isHyperShift {
+				return false
+			}
+			isSNO, precheckSucceeded, err := staticcontrollercommon.NewIsSingleNodePlatformFn(guestConfigInformers.Config().V1().Infrastructures())()
 			if err != nil {
 				klog.Errorf("NewIsSingleNodePlatformFn failed: %v", err)
 				return false
@@ -116,33 +158,64 @@ func RunOperator(ctx context.Context, controllerConfig *controllercmd.Controller
 			}
 			return isSNO
 		},
-	).AddKubeInformers(kubeInformersForNamespaces)
+	).AddKubeInformers(managementKubeInformersForNamespaces)
 
-	controllerDeploymentManifest, err := assets.ReadFile("csi_controller_deployment.yaml")
+	controllerDeploymentManifest, err := namespacedAssetFunc("csi_controller_deployment.yaml")
 	if err != nil {
 		return err
+	}
+
+	var webhookHooks []validatingWebhookConfigHook
+	if isHyperShift {
+		webhookHooks = []validatingWebhookConfigHook{
+			hyperShiftSetWebhookService(),
+		}
+	}
+	webhookController := NewValidatingWebhookConfigController(
+		"WebhookController",
+		guestOperatorClient,
+		guestKubeClient,
+		guestKubeInformersForNamespaces.InformersFor("").Admissionregistration().V1().ValidatingWebhookConfigurations(),
+		namespacedAssetFunc,
+		"webhook_config.yaml",
+		controllerConfig.EventRecorder,
+		webhookHooks,
+	)
+
+	var deploymentHooks []dc.DeploymentHookFunc
+	if isHyperShift {
+		deploymentHooks = []dc.DeploymentHookFunc{
+			hyperShiftReplaceNamespaceHook(operandNamespace),
+			hyperShiftRemoveNodeSelector(),
+			hyperShiftAddKubeConfigVolume("admin-kubeconfig"), // TODO: use dedicated secret for Snapshots
+		}
+	} else {
+		// Standalone OCP
+		deploymentHooks = []dc.DeploymentHookFunc{
+			csidrivercontrollerservicecontroller.WithControlPlaneTopologyHook(guestConfigInformers),
+			csidrivercontrollerservicecontroller.WithReplicasHook(
+				guestKubeInformersForNamespaces.InformersFor("").Core().V1().Nodes().Lister(),
+			),
+		}
 	}
 	controllerDeploymentController := dc.NewDeploymentController(
 		"CSISnapshotController",
 		controllerDeploymentManifest,
 		controllerConfig.EventRecorder,
-		operatorClient,
-		kubeClient,
-		kubeInformersForNamespaces.InformersFor(operatorNamespace).Apps().V1().Deployments(),
+		guestOperatorClient,
+		managementKubeClient,
+		managementKubeInformersForNamespaces.InformersFor(operandNamespace).Apps().V1().Deployments(),
 		[]factory.Informer{
-			kubeInformersForNamespaces.InformersFor("").Core().V1().Nodes().Informer(),
-			configInformers.Config().V1().Infrastructures().Informer(),
+			guestKubeInformersForNamespaces.InformersFor("").Core().V1().Nodes().Informer(),
+			guestConfigInformers.Config().V1().Infrastructures().Informer(),
 		},
 		[]dc.ManifestHookFunc{
 			replacePlaceholdersHook(os.Getenv(operandImageEnvName)),
 		},
-		csidrivercontrollerservicecontroller.WithControlPlaneTopologyHook(configInformers),
-		csidrivercontrollerservicecontroller.WithReplicasHook(
-			kubeInformersForNamespaces.InformersFor("").Core().V1().Nodes().Lister(),
-		),
+		deploymentHooks...,
 	)
 
-	webhookDeploymentManifest, err := assets.ReadFile("webhook_deployment.yaml")
+	webhookDeploymentManifest, err := namespacedAssetFunc("webhook_deployment.yaml")
 	if err != nil {
 		return err
 	}
@@ -152,25 +225,22 @@ func RunOperator(ctx context.Context, controllerConfig *controllercmd.Controller
 		"CSISnapshotWebhookController",
 		webhookDeploymentManifest,
 		controllerConfig.EventRecorder,
-		operatorClient,
-		kubeClient,
-		kubeInformersForNamespaces.InformersFor(operatorNamespace).Apps().V1().Deployments(),
+		guestOperatorClient,
+		managementKubeClient,
+		managementKubeInformersForNamespaces.InformersFor(operandNamespace).Apps().V1().Deployments(),
 		[]factory.Informer{
-			kubeInformersForNamespaces.InformersFor("").Core().V1().Nodes().Informer(),
-			configInformers.Config().V1().Infrastructures().Informer(),
+			guestKubeInformersForNamespaces.InformersFor("").Core().V1().Nodes().Informer(),
+			guestConfigInformers.Config().V1().Infrastructures().Informer(),
 		},
 		[]dc.ManifestHookFunc{
 			replacePlaceholdersHook(os.Getenv(webhookImageEnvName)),
 		},
-		csidrivercontrollerservicecontroller.WithControlPlaneTopologyHook(configInformers),
-		csidrivercontrollerservicecontroller.WithReplicasHook(
-			kubeInformersForNamespaces.InformersFor("").Core().V1().Nodes().Lister(),
-		),
+		deploymentHooks...,
 	)
 
 	versionController := NewVersionController(
 		"VersionController",
-		operatorClient,
+		guestOperatorClient,
 		versionGetter,
 		controllerConfig.EventRecorder,
 		"CSISnapshotControllerAvailable",
@@ -182,13 +252,12 @@ func RunOperator(ctx context.Context, controllerConfig *controllercmd.Controller
 	clusterOperatorStatus := status.NewClusterOperatorStatusController(
 		targetName,
 		[]configv1.ObjectReference{
-			{Resource: "namespaces", Name: targetNamespace},
-			{Resource: "namespaces", Name: operatorNamespace},
+			{Resource: "namespaces", Name: defaultOperandNamespace},
 			{Group: operatorv1.GroupName, Resource: "csisnapshotcontrollers", Name: "cluster"},
 		},
-		configClient.ConfigV1(),
-		configInformers.Config().V1().ClusterOperators(),
-		operatorClient,
+		guestConfigClient.ConfigV1(),
+		guestConfigInformers.Config().V1().ClusterOperators(),
+		guestOperatorClient,
 		versionGetter,
 		controllerConfig.EventRecorder,
 	)
@@ -196,7 +265,7 @@ func RunOperator(ctx context.Context, controllerConfig *controllercmd.Controller
 	// This is the only controller that sets Upgradeable condition
 	cndController := NewConditionController(
 		"ConditionController",
-		operatorClient,
+		guestOperatorClient,
 		controllerConfig.EventRecorder,
 		[]operatorv1.OperatorCondition{
 			{
@@ -207,8 +276,8 @@ func RunOperator(ctx context.Context, controllerConfig *controllercmd.Controller
 		},
 	)
 
-	logLevelController := loglevel.NewClusterOperatorLoggingController(operatorClient, controllerConfig.EventRecorder)
-	managementStateController := managementstatecontroller.NewOperatorManagementStateController(targetName, operatorClient, controllerConfig.EventRecorder)
+	logLevelController := loglevel.NewClusterOperatorLoggingController(guestOperatorClient, controllerConfig.EventRecorder)
+	managementStateController := managementstatecontroller.NewOperatorManagementStateController(targetName, guestOperatorClient, controllerConfig.EventRecorder)
 	management.SetOperatorNotRemovable()
 
 	klog.Info("Starting the Informers.")
@@ -216,8 +285,9 @@ func RunOperator(ctx context.Context, controllerConfig *controllercmd.Controller
 		Start(stopCh <-chan struct{})
 	}{
 		dynamicInformers,
-		configInformers,
-		kubeInformersForNamespaces,
+		guestConfigInformers,
+		managementKubeInformersForNamespaces,
+		guestKubeInformersForNamespaces,
 	} {
 		informer.Start(ctx.Done())
 	}
@@ -229,13 +299,17 @@ func RunOperator(ctx context.Context, controllerConfig *controllercmd.Controller
 		clusterOperatorStatus,
 		logLevelController,
 		managementStateController,
-		staticResourcesController,
 		controllerDeploymentController,
 		webhookDeploymentController,
 		versionController,
 		cndController,
+		guestStaticResourceController,
+		mgmtStaticResourcesController,
+		webhookController,
 	} {
-		go controller.Run(ctx, 1)
+		if controller != nil {
+			go controller.Run(ctx, 1)
+		}
 	}
 
 	<-ctx.Done()
@@ -253,5 +327,79 @@ func replacePlaceholdersHook(imageName string) dc.ManifestHookFunc {
 
 		replaced := strings.NewReplacer(pairs...).Replace(string(manifest))
 		return []byte(replaced), nil
+	}
+}
+
+func hyperShiftReplaceNamespaceHook(operandNamespace string) dc.DeploymentHookFunc {
+	return func(_ *operatorv1.OperatorSpec, deployment *appsv1.Deployment) error {
+		deployment.Namespace = operandNamespace
+		return nil
+	}
+}
+
+func hyperShiftRemoveNodeSelector() dc.DeploymentHookFunc {
+	return func(_ *operatorv1.OperatorSpec, deployment *appsv1.Deployment) error {
+		deployment.Spec.Template.Spec.NodeSelector = map[string]string{}
+		return nil
+	}
+}
+
+func hyperShiftAddKubeConfigVolume(secretName string) dc.DeploymentHookFunc {
+	return func(_ *operatorv1.OperatorSpec, deployment *appsv1.Deployment) error {
+		// inject --kubeconfig arg
+		controllerSpec := &deployment.Spec.Template.Spec
+		controllerSpec.Containers[0].Args = append(controllerSpec.Containers[0].Args, "--kubeconfig=/etc/kubernetes/kubeconfig")
+
+		// Inject Secrets volume with the kubeconfig + its mount
+		kubeConfigVolume := v1.Volume{
+			Name: "kubeconfig",
+			VolumeSource: v1.VolumeSource{
+				Secret: &v1.SecretVolumeSource{
+					// TODO: use a snapshot-controller specific kubeconfig
+					SecretName: secretName,
+				},
+			},
+		}
+		if controllerSpec.Volumes == nil {
+			controllerSpec.Volumes = []v1.Volume{}
+		}
+		controllerSpec.Volumes = append(controllerSpec.Volumes, kubeConfigVolume)
+
+		kubeConfigMount := v1.VolumeMount{
+			Name:      "kubeconfig",
+			ReadOnly:  true,
+			MountPath: "/etc/kubernetes",
+		}
+		if controllerSpec.Containers[0].VolumeMounts == nil {
+			controllerSpec.Containers[0].VolumeMounts = []v1.VolumeMount{}
+		}
+		controllerSpec.Containers[0].VolumeMounts = append(controllerSpec.Containers[0].VolumeMounts, kubeConfigMount)
+
+		return nil
+	}
+}
+
+func hyperShiftSetWebhookService() validatingWebhookConfigHook {
+	return func(configuration *admissionregistrationv1.ValidatingWebhookConfiguration) error {
+		for i := range configuration.Webhooks {
+			webhook := &configuration.Webhooks[i]
+			webhook.ClientConfig.Service = nil
+			// Name of csi-snapshot-webhook service in the same namespace as the guest API server.
+			url := "https://csi-snapshot-webhook"
+			webhook.ClientConfig.URL = &url
+		}
+
+		return nil
+	}
+}
+
+func namespaceReplacer(assetFunc resourceapply.AssetFunc, namespace string) resourceapply.AssetFunc {
+	return func(name string) ([]byte, error) {
+		asset, err := assetFunc(name)
+		if err != nil {
+			return asset, err
+		}
+		asset = bytes.ReplaceAll(asset, []byte("${NAMESPACE}"), []byte(namespace))
+		return asset, nil
 	}
 }
